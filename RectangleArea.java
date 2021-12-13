0f76950a4eee3304351d78153280837088e3fca0
bsc-com-10-19 <bsc-com-10-19@unima.ac.mw>
2021-12-13 16:59:18 +0200

    the area program has been written

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,5 +1,26 @@
 publi class RectangleArea{
 public static void main(String[]args){
+Scanner input = new Scanner(System.in);
 System.out.println("the area will be shown here");
+System.out.print("Enter length and breadth: ");
+int length = input.nextInt();
+int breadth = input.nextInt();
+
+Area rectangleArea = new Area(length,breadth);
+System.out.println("the area of the rectangle of length"+ 
+  length +" and breadth of " + breadth + " is" + rectangleArea.getArea());
+}
+}
+class Area{
+int length;
+int breadth;
+  
+  public Area(int length,int breadth){
+   this.length = length;
+   this.breadth = breadth;
+}
+public double getArea(){
+  double area = length * breadth;
+   return area;
 }
 }
